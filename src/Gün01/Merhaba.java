be517d2f75345ad6dae7eb019a96c83630d02d63
MDU <mdu9506@gmail.com>
2025-08-04 10:20:59 +0300

    terminalden commit and push yapıldı

--- src/Gün01/Merhaba.java
+++ src/Gün01/Merhaba.java
@@ -3,5 +3,6 @@ package Gün01;
 public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        System.out.println("SDET eğitimi başıyor");
     }
 }
